7b5db5d6c34bd9dc518e7d5f10efcea957547683
yavor300 <yavor300@gmail.com>
2021-07-17 15:17:44 +0300

    Changed the secret key to be UUID *educational purposes

--- student-management-domain/src/main/java/project/restapi/constants/SecurityValues.java
+++ student-management-domain/src/main/java/project/restapi/constants/SecurityValues.java
@@ -1,5 +1,5 @@
 package project.restapi.constants;
 
 public class SecurityValues {
-    public static final String SECRET_KEY = "secret2";
+    public static final String SECRET_KEY = "54bdfc5a-0023-4671-81c6-8e28eaed36c5";
 }
